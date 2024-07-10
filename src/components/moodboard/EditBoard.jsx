14c41e792bd7a7a0ee595756737e874c12f82b38
dp-daly <dan.daly@education.gov.uk>
2024-07-10 10:16:24 +0100

    All CRUD tested and working

--- src/components/moodboard/EditBoard.jsx
+++ src/components/moodboard/EditBoard.jsx
@@ -1,13 +1,104 @@
 import '../../styles/App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function EditBoard() {
+    const { boardId } = useParams()  
+    const navigate = useNavigate()
 
-    return (
-        <div className="container">
-            <p>Hello</p>
-        </div>
-    )
+    const [formData, setFormData] = useState({
+        title: '',
+        description: '',
+    })
 
+    useEffect(() => {
+        fetchBoard()
+    }, [boardId])
+
+async function fetchBoard() {
+    try {
+        const token = localStorage.getItem('token')
+        const { data } = await axios.get(`http://localhost:8000/api/boards/${boardId}/`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        setFormData(data)
+    } catch (err) {
+        console.log(err.response.data)
+        toast.error("Cannot load this board's information.")
+    }
 }
 
-export default EditBoard;
+function handleChange(e) {
+    const newFormData = structuredClone(formData)
+    newFormData[e.target.name] = e.target.value
+    setFormData(newFormData)
+    }
+
+async function handleSubmit(e) {
+    e.preventDefault()
+    const token = localStorage.getItem('token')
+    try {
+        await axios.put(`http://localhost:8000/api/boards/${boardId}/`, formData, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        navigate('/')
+    } catch (err) {
+        console.log(err.response.data)
+        toast.error("Try again - something went wrong.")
+    }
+}
+
+    return <div className="container">
+            <form onSubmit={handleSubmit}>
+                <div className="field">
+                    <label className="label">Title</label>
+                    <div className="control">
+                        <input
+                            className="input"
+                            type="text"
+                            name={'title'}
+                            onChange={handleChange}
+                            value={formData.title}
+                            autoComplete="off"
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Description</label>
+                    <div className="control">
+                        <input
+                            className="input"
+                            type="text"
+                            name={'description'}
+                            onChange={handleChange}
+                            value={formData.description}
+                            autoComplete="off"
+                        />
+                    </div>
+                </div>
+                <button className="button">Submit</button>
+            </form>
+            <ToastContainer
+                position="bottom-center"
+                autoClose={2000}
+                hideProgressBar
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+                toastStyle={{ backgroundColor: "black", color: "white" }}
+            />
+        </div>
+}
+
+export default EditBoard
--- src/components/moodboard/Moodboard.jsx
+++ src/components/moodboard/Moodboard.jsx
@@ -1,8 +1,12 @@
+import '../../styles/App.css'
 import { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 
 function Moodboard() {
+
+    const navigate = useNavigate()
+
     const [moodboard, setMoodboard] = useState(null);
     const { boardId } = useParams()
 
@@ -10,14 +14,38 @@ function Moodboard() {
         fetchMoodboard();
     }, [boardId]);
 
-    async function fetchMoodboard() {
-        try {
-            const response = await axios.get(`http://localhost:8000/api/boards/${boardId}/`);
-            setMoodboard(response.data);
-        } catch (error) {
-            console.log('Error fetching moodboard:', error.message);
-        }
+async function fetchMoodboard() {
+    try {
+        const response = await axios.get(`http://localhost:8000/api/boards/${boardId}/`);
+        setMoodboard(response.data);
+    } catch (error) {
+        console.log('Error fetching moodboard:', error.message);
     }
+}
+
+async function handleBoardDelete() {
+    try {
+        const token = localStorage.getItem("token")
+        await axios.delete(`http://localhost:8000/api/boards/${boardId}/`, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        navigate('/')
+    } catch (err) {
+        console.log(err)
+    }
+}
+    
+async function handleItemDelete(objectId) {
+    try {
+        const token = localStorage.getItem("token");
+        await axios.delete(`http://localhost:8000/api/boards/${boardId}/${objectId}/`, {
+            headers: { Authorization: `Bearer ${token}` }
+        });
+        fetchMoodboard();
+    } catch (err) {
+        console.log(err);
+    }
+}
 
     return (
         <div className="container">
@@ -26,21 +54,28 @@ function Moodboard() {
                     <h2>{moodboard.title}</h2>
                     <p>{moodboard.description}</p>
 
-                    <Link to={`${location.pathname}/edit`} className="button" id="create">Edit Board</Link>
+                    <Link to={`${location.pathname}/edit`} className="button" id="edit">Edit Board</Link>
+                    <Link to="/" className="button" id="delete" onClick={handleBoardDelete}>Delete board</Link>
                     <h3>Art Objects:</h3>
                     <ul>
                         {moodboard.artobjects && moodboard.artobjects.map((artobject) => (
                             <li key={artobject.id}>
                                 <h4>{artobject.title}</h4>
                                 <p>Artist: {artobject.artist}</p>
-                                <img src={artobject.img} alt={artobject.title} style={{ maxWidth: '200px' }} />
+                                <div 
+                                    id="img-placeholder" 
+                                    style={{ 
+                                        backgroundImage: `url(https://www.artic.edu/iiif/2/${artobject.img}/full/843,/0/default.jpg)`,
+                                    }}
+                                ></div>
+                                <button onClick={() => handleItemDelete(artobject.id)}>Delete</button>
                             </li>
                         ))}
                     </ul>
                 </div>
             )}
         </div>
-    );
+    )
 }
 
 export default Moodboard;
